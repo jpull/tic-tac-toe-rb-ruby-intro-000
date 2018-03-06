5df6ea429f3b0233f6fb0a757fb749ff3d1b8984
Jerry P <jpull@jpull.com>
2018-03-06 04:24:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,9 +21,33 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
 
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
 
 
 # Helper Method
@@ -31,9 +55,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
+
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
