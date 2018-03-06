4304ac71501f1824ad4b5060a03f81654164bcd0
Jerry P <jpull@jpull.com>
2018-03-06 04:31:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index,turn_count(board))
     display_board(board)
   end
 end
