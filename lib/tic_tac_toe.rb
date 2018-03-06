72d7b52d04576f636f4c71505e16e2023e916649
Jerry P <jpull@jpull.com>
2018-03-06 04:25:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,19 +53,6 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-
-
-
-
-
-
-
-
-# Define your WIN_COMBINATIONS constant
-
-
-
-
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
